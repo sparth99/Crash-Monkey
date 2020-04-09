a0557981fe70422a33951b80a911df3fd63828a9
sparth99 <sparth99@gmail.com>
2020-04-09 14:44:50 -0500

    added ipynb file for pora

--- backend/data-prep/process.py
+++ backend/data-prep/process.py
@@ -1,2 +1,8 @@
-import pandas
+import pandas as pd
+from sqlalchemy import create_engine
 
+engine = create_engine('postgresql+psycopg2://postgres://pshah:hello@localhost:5432/chaosmonkey')
+
+df = pd.read_sql_query('select * from "Logs"',con=engine)
+
+print(df)
